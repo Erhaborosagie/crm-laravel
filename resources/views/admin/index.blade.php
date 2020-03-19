da1e9ae4129c852639d7007d4758ed15bd3f714a
Osagie Erhabor <erhaborosagie@gmail.com>
2020-03-19 21:00:00 +0100

    Created admin view

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,33 @@
+@extends("admin.layouts.app");
+
+@section("content")
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-2">
+                <div class="card">
+                    <div class="card-header">Dashboard</div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item"><a href="#">Leads</a></li>
+                        <li class="list-group-item"><a href="#">People</a></li>
+                        <li class="list-group-item"><a href="#">Companies</a></li>
+                        <li class="list-group-item"><a href="#">Opportunities</a></li>
+                        <li class="list-group-item"><a href="#">Projects</a></li>
+                        <li class="list-group-item"><a href="#">Tasks</a></li>
+                        <li class="list-group-item"><a href="#">Emails</a></li>
+                        <li class="list-group-item"><a href="#">Reports</a></li>
+                        <li class="list-group-item"><a href="#">Settings</a></li>
+                    </ul>
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+
+                        You are logged in!
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
